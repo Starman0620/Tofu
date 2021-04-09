6bf5218da1a1b87c7c17799377fa0153396b47a8
Starman0620 <CamdenK2006@gmail.com>
2021-04-09 18:11:57 -0400

    Changed serverstats to display only past 14 days

--- TofuBot/Source/Commands/Staff/ServerStatsCommand.cs
+++ TofuBot/Source/Commands/Staff/ServerStatsCommand.cs
@@ -16,20 +16,21 @@ using TofuBot.Utils;
 
 namespace TofuBot.Commands
 {
-	public class ServerStatsCommand : ModuleBase<SocketCommandContext>
-	{
-		[Command("serverstats")]
-		[Priority(Category.Staff)]
-		[Summary("Show basic statistics about the server|")]
-		public async Task ServerStats([Remainder]string args = null)
-		{
-			// Report loading
+    public class ServerStatsCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("serverstats")]
+        [Priority(Category.Staff)]
+        [Summary("Show basic statistics about the server|")]
+        public async Task ServerStats([Remainder] string args = null)
+        {
+            // Report loading
             List<DailyReport> reports = new List<DailyReport>();
-            foreach(string file in Directory.GetFiles("DailyReports")) {
+            foreach (string file in Directory.GetFiles("DailyReports"))
+            {
                 DailyReport newReport = JsonConvert.DeserializeObject<DailyReport>(File.ReadAllText(file));
                 reports.Add(newReport);
             }
-			reports = reports.OrderByDescending(grp => grp.DayOfReport.DayOfYear).Reverse().ToList();
+            reports = reports.OrderByDescending(grp => grp.DayOfReport.DayOfYear).Reverse().ToList();
 
             // Data parsing
             double[] messages = new double[reports.Count];
@@ -37,7 +38,8 @@ namespace TofuBot.Commands
             double[] userJoin = new double[reports.Count];
             double[] userLeave = new double[reports.Count];
             string[] xticks = new string[reports.Count];
-            for(int i = 0; i < reports.Count; i++) {
+            for (int i = 0; i < reports.Count; i++)
+            {
                 messages[i] += reports[i].MessagesSent;
                 commands[i] += reports[i].CommandsRan;
                 userJoin[i] += reports[i].UsersJoined;
@@ -45,15 +47,27 @@ namespace TofuBot.Commands
             }
 
             double[] ys = new double[reports.Count];
-            for(int i = 0; i < reports.Count; i++) {
-                ys[i] = i;
-                xticks[i] = (i+1).ToString();
+            if (reports.Count > 15)
+            {
+                for (int i = reports.Count - 1; i > reports.Count - 15; i--)
+                {
+                    ys[i] = i;
+                    xticks[i] = reports[i].DayOfReport.ToShortDateString();
+                }
             }
-            
+            else
+            {
+                for(int i = 0; i < reports.Count; i++)
+                {
+                    ys[i] = i;
+                    xticks[i] = reports[i].DayOfReport.ToShortDateString();
+                }
+            }
+
             // Plotting
             Plot plt = new Plot(1920, 1080);
             plt.Style(System.Drawing.Color.FromArgb(52, 54, 60), System.Drawing.Color.FromArgb(52, 54, 60), null, System.Drawing.Color.White, System.Drawing.Color.White, System.Drawing.Color.White);
-            plt.XLabel("D a y s  S i n c e  C r e a t i o n", null, null, null, 25.5f, false);
+            plt.XLabel("D a y", null, null, null, 25.5f, false);
             plt.YLabel("G o o d  D a t a", null, null, 25.5f, null, false);
             plt.PlotFillAboveBelow(ys, messages, "Messages", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(100, 119, 183), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(100, 119, 183), fillColorAbove: System.Drawing.Color.FromArgb(100, 119, 183));
             plt.PlotFillAboveBelow(ys, commands, "Command Executions", lineWidth: 4, lineColor: System.Drawing.Color.FromArgb(252, 186, 3), fillAlpha: .5, fillColorBelow: System.Drawing.Color.FromArgb(252, 186, 3), fillColorAbove: System.Drawing.Color.FromArgb(252, 186, 3));
@@ -65,28 +79,30 @@ namespace TofuBot.Commands
             plt.Legend(true, null, 30, null, null, System.Drawing.Color.FromArgb(100, 52, 54, 60), null, legendLocation.upperRight, shadowDirection.lowerRight, null, null);
 
             // Save and send
-			plt.SaveFig("stats.png");
-			if(args != null) {
-				await Context.Channel.SendFileAsync("stats.png");
-				return;
-			}
+            plt.SaveFig("stats.png");
+            if (args != null)
+            {
+                await Context.Channel.SendFileAsync("stats.png");
+                return;
+            }
 
-			// Draw it onto C H A D
-			Bitmap chad = new Bitmap(Bitmap.FromFile("chad.png"));
-			Bitmap stats = new Bitmap(Bitmap.FromFile("stats.png"));
-			using(Graphics canvas = Graphics.FromImage(chad)) {
-				canvas.DrawImage(stats, graphPos);
-				canvas.Save();
-			}
+            // Draw it onto C H A D
+            Bitmap chad = new Bitmap(Bitmap.FromFile("chad.png"));
+            Bitmap stats = new Bitmap(Bitmap.FromFile("stats.png"));
+            using (Graphics canvas = Graphics.FromImage(chad))
+            {
+                canvas.DrawImage(stats, graphPos);
+                canvas.Save();
+            }
 
-			chad.Save("stats.png");
-			await Context.Channel.SendFileAsync("stats.png");
-		}
+            chad.Save("stats.png");
+            await Context.Channel.SendFileAsync("stats.png");
+        }
 
-		static Point[] graphPos = new Point[] {
-			new Point(900, 500),  // Top left
+        static Point[] graphPos = new Point[] {
+            new Point(900, 500),  // Top left
 			new Point(1440, 385), // Top right
 			new Point(985, 890),  // Bottom left
 		};
-	}
+    }
 }
